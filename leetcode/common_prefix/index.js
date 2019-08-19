e9289aa915eee9422359795a8adfd0c03fa6647c
xu-126 <1264930795@qq.com>
2019-08-19 20:49:09 +0800

    leetcode/common_prefix

---
+++ leetcode/common_prefix/index.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+function isCommonPrefix(strs,middle) {
+    const prefix = strs[0].substring(0,middle);
+    for (let i = 1; i < strs.length;i++){
+        if(!strs[i].startsWith(prefix)) return false;
+    }
+    return true;
+}
+var longestCommonPrefix = function(strs) {
+    // 最短的一项
+    let minLen = Number.MAX_VALUE;
+    for(let i = 0;i<strs.length; i++) {
+        minLen = Math.min(minLen,strs[i].length);
+    }
+    console.log(minLen);
+    // 二分
+    let low = 0; //start
+    high = minLen;//end
+    // 不停的查找中间值
+    // let mid = Math.floor((low + high)/2);
+    while(low <= high){
+        let mid = (low + high)>>1;  // >>右移一位
+        if (isCommonPrefix(strs,mid)) low = mid + 1; //后半段
+        else high = mid - 1; //前半段
+    }   
+    return strs[0].substring(0,(low+high)>>1)
+}
+console.log(longestCommonPrefix(["flower","flow","flight"]));
