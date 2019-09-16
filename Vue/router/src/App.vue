424aca0d4eacc605fa21a40892aa4912f65a8ff4
xu-126 <1264930795@qq.com>
2019-09-16 17:19:29 +0800

    Vue/router

--- Vue/router/src/App.vue
+++ Vue/router/src/App.vue
@@ -2,6 +2,13 @@
   <div id="app">
     <img src="./assets/logo.png">
     <router-view/>
+    <p>导航：
+      <router-link to="/">首页</router-link>
+      <router-link :to="{name:'Look',params:{id: 123}}">看一看</router-link>
+      <router-link to="/look/look1">看1</router-link>
+      <router-link to="/look/look2">看2</router-link>
+      <router-link to="/go-back">重定向</router-link>
+    </p>
   </div>
 </template>
 
--- Vue/router/src/router/index.js
+++ Vue/router/src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Look from '@/components/Look'
+import Look1 from '@/components/Look1'
+import Look2 from '@/components/Look2'
 
 Vue.use(Router)
 
@@ -10,6 +13,27 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/look',
+      name: 'Look',
+      component: Look,
+      children:[
+        {
+          path:'/look/look1',
+          component:Look1
+        },
+        {
+          path:'/look/look2',
+          name:'Look2',
+          component:Look2
+        }
+      ]
+    },
+    {
+      path:'/go-back',
+      redirect:'/'
     }
   ]
-})
+}
+)
