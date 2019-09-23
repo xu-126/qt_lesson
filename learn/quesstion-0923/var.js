52a2ad739f055f4a35a3b5748a1e9d6bbc596d09
xu-126 <1264930795@qq.com>
2019-09-23 17:04:39 +0800

    learn/

---
+++ learn/quesstion-0923/var.js
@@ -0,0 +1,30 @@
+console.log(a)
+var a = 10;    
+function a() {
+    console.log(123)
+}
+console.log(a)
+// function 声明和赋值都会提升
+// var 声明 提升 赋值不会
+// function a
+// var a
+// console.log()
+// a=10
+var x = 1,
+      y = 0,
+      z = 0;
+    function add(x) {
+      return (x = x + 1);
+    }
+    y = add(x); 
+    console.log(y)
+    function add(x) {
+      return (x = x + 3);
+    }
+    z = add(x); 
+    console.log(z);
+    console.log(x);
+
+    function foo(a){
+        a=100;
+    }
