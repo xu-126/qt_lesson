7c9106110ba26ea21f96b07e215e80396cc9f6b7
xu-126 <1264930795@qq.com>
2019-11-08 19:20:32 +0800

    React/router-juejin/

---
+++ React/router-juejin/src/App.js
@@ -0,0 +1,69 @@
+import React,{Component} from 'react';
+import reactDom from 'react-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
+import { createStore } from 'redux'
+import './App.css';
+import Layout from './Layout'
+
+// 决定Store 长什么样
+// state 就是上一份 state 
+// action 就是提交过来的
+function reducer(state,action){
+  if(!state) {
+    return {
+      count : 4
+    }
+  }
+  const { count } = state
+  if(action.type === 'ADD'){
+    return {
+      count : count + 1
+    }
+  } 
+  if(action.type === 'SUB'){
+    return {
+      count : count - 1
+    }
+  }
+}
+const store = createStore(reducer);
+
+class Counter extends Component {
+  add = () => {
+    store.dispatch({
+      type: 'ADD'
+    })
+  }
+  sub = () =>{
+    store.dispatch({
+      type: 'SUB'
+    })
+  }
+  render() {
+    console.log('store' + store.getState().count)
+    return (
+      <div>
+        count : { store.getState().count}
+        <button onClick={this.add}>+</button>
+        <button onClick={this.sub}>-</button>
+      </div>
+    )
+  }
+}
+store.subscribe(() => {
+  reactDom.render(<Counter />, document.getElementById('root'))
+})
+reactDom.render(<Counter />, document.getElementById('root'))
+
+// react-router 1. dom  2. native  3. 内存
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Route path="/" component={Layout} />
+      </BrowserRouter>
+    )
+  }
+}
+
+export default App;
