cd7f058eea2f7699d9bd6b63b94ab8fd35af22d7
xu-126 <1264930795@qq.com>
2019-11-18 21:41:00 +0800

    vue-cli

---
+++ Vue/vue-cli/my-app/src/pages/home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="home">
+      这是一个home
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'home'
+}
+</script>
+
+<style>
+
+</style>
---
+++ Vue/vue-cli/my-app/src/pages/login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="login">
+      这是一个login
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'login'
+}
+</script>
+
+<style>
+
+</style>
