1111dd080f17b6562d0b3111cb918a93d1635f38
xu-126 <1264930795@qq.com>
2019-11-19 16:06:07 +0800

    Vue/vue-cli/

--- Vue/vue-cli/my-app/src/App.vue
+++ Vue/vue-cli/my-app/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="id">
+      <img src="./assets/2.jpg" alt="">
       <router-link to="/login">登录</router-link>
       <router-link to="/">home</router-link>
       <router-view></router-view>
@@ -7,11 +8,20 @@
 </template>
 
 <script>
+
+// createElement('img',{
+//   attrs: {
+//     src: require("./assets/2.jpg")
+//   }
+// })
 export default {
     name: 'app'
 }
 </script>
 
-<style>
-
+<style lang="less">
+body{
+  height:500px;
+  background:red;
+}
 </style>
