1a2671a6489453f85985adf9d5ec2db4edb61276
xu-126 <1264930795@qq.com>
2019-11-22 21:15:50 +0800

    React/react-immutable/

---
+++ React/react-immutable/README.old.md
@@ -0,0 +1,1 @@
+cnpm i immutable -S
---
+++ React/react-immutable/src/App.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import immutable from 'immutable'
+import logo from './logo.svg';
+import './App.css';
+
+// 可变
+let a = [0,1,2]
+a.push(3)
+// 不可变
+let immutableArr = immutable.fromJS(
+  [2,3,4]
+  )
+let b = immutableArr.push(5)
+console.log(b.toJS())
+
+let obj = { a: 1 , 
+            b: {
+              c:2
+            },
+            d: {
+              e:3
+            }
+          }
+let imObj = immutable.fromJS(obj)
+console.log(imObj.getIn(['b','c'],'default'))
+let imObj = imObj.setIn(['d','e'],'ee')
+let obj1 = {
+  ...obj,
+  b : 2
+}
+console.log(obj)
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
