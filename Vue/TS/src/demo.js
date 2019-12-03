8a0c52eab2773f10bc51d673ba8032481d0ae03c
xu-126 <1264930795@qq.com>
2019-12-03 20:55:50 +0800

    Vue/TS

---
+++ Vue/TS/src/demo.js
@@ -0,0 +1,7 @@
+function greeter(person) {
+    // console.log(a);
+    return "Hello, " + person;
+}
+var user = '[1,2,3]';
+var a = greeter(user);
+console.log(a);
---
+++ Vue/TS/src/demo.ts
@@ -0,0 +1,10 @@
+function greeter(person: string) {
+    console.log(a)
+    return "Hello, " + person;  
+}
+
+let user = '[1,2,3]';
+
+const a = greeter(user);
+
+console.log(a)
